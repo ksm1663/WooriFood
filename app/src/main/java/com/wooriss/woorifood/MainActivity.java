c00e9c2001c6fcb4cdc9ff6cbef4db80e0fab06b
ksm1663 <ksm1663@gmail.com>
2021-10-07 02:16:54 +0900

    commit test

--- app/src/main/java/com/wooriss/woorifood/MainActivity.java
+++ app/src/main/java/com/wooriss/woorifood/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.dinuscxj.progressbar.CircleProgressBar;
 
-// 커밋테스트20211007
 
 public class MainActivity extends AppCompatActivity {
 
